21f7ca0a28a6e5dbf70d90c4d4b664627e24932e
thilo-cts <thilojith@gmail.com>
2017-06-05 15:46:12 +0530

    Added default route

--- app/routes/dashboard/show.js
+++ app/routes/dashboard/show.js
@@ -1,9 +1,8 @@
 import Ember from 'ember';
-import _ from "underscore";
 export default Ember.Route.extend({
     model(params){
         var dashboard = this.modelFor('dashboard');       
-        dashboard = _.findWhere(dashboard, {boxId : parseInt(params.boxId)});
+        dashboard = dashboard.findBy('boxId', parseInt(params.boxId));
         return dashboard;
     }
 });
