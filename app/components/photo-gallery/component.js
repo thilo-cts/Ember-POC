936255f2b21579199119dbf0a04ed4ebdc7dbe25
thilo-cts <thilojith@gmail.com>
2017-06-16 19:14:47 +0530

    Added gallery route

--- app/components/photo-gallery/component.js
+++ app/components/photo-gallery/component.js
@@ -1,5 +1,20 @@
 import layout from './template';
 export default Ember.Component.extend({
-layout: layout
+layout: layout,
+store: Ember.inject.service('servlet'),
+init(){
+       this._super(...arguments);
+        this.getAllPhotoLists();
+},
+getAllPhotoLists(){
+    let store = this.get('store');
+    store.getListOfPhotos().then((response) => {
+        console.log("at init.........", response);
+         this.set('photos', response);
+    });
+},
+actions: {
+
+}
 
 })
--- app/components/welcome-container/component.js
+++ app/components/welcome-container/component.js
@@ -1,6 +1,25 @@
 import layout from './template';
 
 export default Ember.Component.extend({
-layout: layout
+layout: layout,
+store: Ember.inject.service('servlet'),
+init() {
+   this._super(...arguments);
+   this.set('photos', 'ihjk');
+   this.getAllPhotoLists();
+},
+getAllPhotoLists(){
+    let store = this.get('store');
+    store.getListOfPhotos().then((response) => {
+        console.log("at init.........", response);
+         this.set('photos', response);
+    });
+},
+actions : {
+    submit : function() {
+        let photos = this.get('photos');
+        this.get('router').transitionTo('welcome.aboutus', {queryParams: {'title': photos}});
+    }
+}
 
 })
